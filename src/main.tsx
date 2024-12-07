c0098984cf23af70741ac277c650155e7a625c9e
saksham.s <sakshams982@gmail.com>
2024-12-07 19:17:39 +0545

    #86eqwdy5a:Defined routes for login and signup page as well as added the logo assest

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+    createBrowserRouter, redirect,
+    RouterProvider,
+} from "react-router-dom";
 import './index.css'
-import App from './App.tsx'
+import AuthLayout from "./layouts/AuthLayout.tsx";
+import SignUpPage from "@/pages/sign-up.page.tsx";
+import SignInPage from "@/pages/sign-in.page.tsx";
+const router=createBrowserRouter(
+    [
+        {
+            path:'/',
+           loader:()=>redirect('/login'),
+        },
+        {
+            element:<AuthLayout/>,
+            children:[
+                {
+                    path:'/sign-up',
+                    element:<SignUpPage/>
+                },
+                {
+                    path:'/login',
+                    element: <SignInPage/>,
+
+                },
+            ]
+        }
+    ]
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
