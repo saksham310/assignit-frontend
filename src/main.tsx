8c9565a4139278e074d9ae8818b37f7620d1208b
saksham.s <sakshams982@gmail.com>
2024-12-12 10:41:24 +0545

    #86eqy2n22:Added QueryClient provider for hooks

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './index.css'
 import AuthLayout from "./layouts/AuthLayout.tsx";
 import SignUpPage from "@/pages/sign-up.page.tsx";
 import SignInPage from "@/pages/sign-in.page.tsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 const router=createBrowserRouter(
     [
         {
@@ -30,9 +31,11 @@ const router=createBrowserRouter(
         }
     ]
 )
-
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+     </QueryClientProvider>
   </StrictMode>,
 )
