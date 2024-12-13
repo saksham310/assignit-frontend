9093952fca1af3378510e82cb5c308a2ba76edef
saksham.s <sakshams982@gmail.com>
2024-12-13 12:12:44 +0545

    Major code refactor

--- src/hooks/authHooks.ts
+++ src/hooks/authHooks.ts
@@ -2,15 +2,16 @@ import {useMutation} from "@tanstack/react-query";
 import {login, register} from "@/service/authService.ts";
 import useSignIn from "react-auth-kit/hooks/useSignIn";
 import {toast} from "sonner";
+import {useNavigate} from "react-router-dom";
+import {AuthHookConfig, AuthResponse, LoginInput, RegisterInput} from "@/types/auth.type.ts";
 
 
 
-export const useLogin=()=>{
+const useAuth=<T extends LoginInput|RegisterInput>({mutationFn,successMessage}:AuthHookConfig<T>)=>{
+    const navigate=useNavigate();
     const signIn = useSignIn();
-    return useMutation({
-        mutationFn: login,
-        onSuccess: (res) => {
-            console.log("data",res);
+    const handleSuccess=(res:AuthResponse)=>{
+        {
             if (signIn({
                 auth: {
                     token: res.token,
@@ -18,31 +19,27 @@ export const useLogin=()=>{
                 },
                 userState: res.user
             })){
-                toast.success("Logged in successfully", {
+                toast.success(successMessage, {
                     duration: 2000,
                 });
+                setTimeout(() => {
+                    navigate('/dashboard');
+                }, 1000);
             }
-                }
-    })
+        }
+    }
+    return useMutation({mutationFn,onSuccess:handleSuccess});
+}
+export const useLogin=()=>{
+    return useAuth<LoginInput>({
+        mutationFn:login,
+        successMessage:"Logged in Successfully"
+    });
 }
 
 export const useRegister=()=>{
-    const signIn = useSignIn();
-    return useMutation({
-        mutationFn: register,
-        onSuccess: (res) => {
-            console.log("data",res);
-            if (signIn({
-                auth: {
-                    token: res.token,
-                    type: 'Bearer'
-                },
-                userState: res.user
-            })){
-                toast.success("Registered successfully", {
-                    duration: 2000,
-                });
-            }
-        }
-    })
+    return useAuth<RegisterInput>({
+        mutationFn:register,
+        successMessage:"Registered successfully"
+    });
 }
