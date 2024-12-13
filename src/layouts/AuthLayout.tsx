c0810773c12b1592c3b1f75fdcca86207ea2e2d6
saksham.s <sakshams982@gmail.com>
2024-12-13 12:11:51 +0545

    Added a condition to check if user is authenticated and redirect based on it

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,12 @@
-import Logo from "@/assets/Logo.svg"
-import { Outlet} from "react-router-dom";
+import Logo from "@/assets/Logo.svg";
+import {Navigate, Outlet} from "react-router-dom";
+import useIsAuthenticated from "react-auth-kit/hooks/useIsAuthenticated";
 
 const AuthLayout=()=>{
+    const isAuthenticated = useIsAuthenticated();
+    if(isAuthenticated){
+      return  <Navigate to="/dashboard" replace={true} />;
+    }else{
     return <>
         <main className="min-h-screen bg-[#EEEBF6] p-4">
             <div className="mx-auto max-w-screen-2xl p-6">
@@ -9,12 +14,12 @@ const AuthLayout=()=>{
                     <img src={Logo} alt="assignIt logo" className='h-12 w-auto'/>
                 </nav>
                 <div className="mt-20 flex items-center justify-center">
-                    <Outlet></Outlet>
+                    <Outlet />
                 </div>
             </div>
         </main>
 
     </>;
-}
+}}
 
 export default AuthLayout;
