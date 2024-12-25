a129fca2146b7cd093585297729a05d904734b3f
saksham.s <sakshams982@gmail.com>
2024-12-25 18:55:54 +0545

    Added a Dashboard component and updated the dashboard wrapper with dummy data [to be replace with actual] for common layout

--- src/layouts/DashboardWrapper.tsx
+++ src/layouts/DashboardWrapper.tsx
@@ -1,23 +1,29 @@
 import Sidebar from "@/components/shared/Sidebar.tsx";
-// import {Outlet} from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import TopBar from "@/components/shared/TopBar.tsx";
 import {useState} from "react";
 import Dashboard from "@/components/shared/Dashboard.tsx";
 
 const DashboardWrapper = () => {
-    const [title,setTitle]=useState<string>("Workspace Summary");
-  return  <>
-      <div className='flex w-full h-full p-4 '>
-<div className='fixed top-[18px] left-[16px] hidden lg:block lg:w-[200px] overflow-y-auto h-full'><Sidebar/></div>
-      </div>
-      <div className='w-full fixed lg:pl-[220px] min-h-[calc(100vh-32px)] overflow-hidden '>
-          <div className="max-w-screen-2xl px-6 mb-4"><TopBar title={title}/></div>
-          <div className="h-[calc(100vh-64px)] overflow-y-auto rounded-lg flex flex-col py-8 px-6 bg-[#f6f8fb]">
-            {/*<Outlet context={setTitle}/>*/}
-              <Dashboard/>
-          </div>
-      </div>
-  </>
-}
+    const [title, setTitle] = useState<string>("Workspace Summary");
+    return (
+        <div className="flex min-h-screen bg-white">
+            <div className="fixed top-0 left-0 h-screen w-[200px] hidden lg:block p-4">
+                <Sidebar />
+            </div>
+            <div className="flex-1 lg:ml-[200px]">
+                <div className="px-6 py-4">
+                    <TopBar title={title} />
+                </div>
+                <div className="px-6 pb-4">
+                    <div className="bg-[#f6f8fb] rounded-lg p-8 lg:h-[calc(100vh-76px)] md:min-h-[calc(100vh-80px)]">
+                        <Outlet context={setTitle} />
+                        <Dashboard />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default DashboardWrapper;
--- src/types/dashboard.type.ts
+++ src/types/dashboard.type.ts
@@ -2,3 +2,10 @@ export interface AnalyticCardProps{
     name: string,
     count:number
 }
+
+export type ProjectTask={
+    taskName:string,
+    sprintName:string,
+    status:string,
+    priority:string
+}
