7e3bc201c1729b8a73f4467e68ce2214a9cddd95
saksham.s <sakshams982@gmail.com>
2024-12-25 18:54:59 +0545

    Created a constants for icon mapping

---
+++ src/constants/icons.constants.ts
@@ -0,0 +1,9 @@
+
+import {FolderOpenDot, Folders, LucideIcon, Paperclip, TriangleAlert, UsersRound} from "lucide-react";
+export const ICONS:Record<string,LucideIcon> = {
+    "Projects":FolderOpenDot,
+    "Sprints":Folders,
+    "Members":UsersRound,
+    "Tasks":Paperclip,
+    "Overdue":TriangleAlert
+};
