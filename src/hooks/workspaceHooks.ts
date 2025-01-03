39d4c3e3ca9467f93d7f5eb1759b24c675aa2e12
saksham.s <sakshams982@gmail.com>
2025-01-03 14:28:00 +0545

    Updated the hook to fetch only if header's available

--- src/hooks/workspaceHooks.ts
+++ src/hooks/workspaceHooks.ts
@@ -1,9 +1,12 @@
 import {getWorkspaces} from "@/service/workspaceService.ts";
 import {useQuery} from "@tanstack/react-query";
+import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
 
 export const useGetWorkspace = () => {
+    const header=useAuthHeader();
     return useQuery({
         queryKey:['workspaces'],
         queryFn:getWorkspaces,
+        enabled:!!header
     });
 }
