87e26e22c59aa6b4087632a41ad13a62eb5f1802
saksham.s <sakshams982@gmail.com>
2025-01-03 22:08:49 +0545

    Added hooks for workspaces

--- src/hooks/workspaceHooks.ts
+++ src/hooks/workspaceHooks.ts
@@ -2,6 +2,7 @@ import {getWorkspaces} from "@/service/workspaceService.ts";
 import {useQuery} from "@tanstack/react-query";
 import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
 import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 export const useGetWorkspace = () => {
     const header=useAuthHeader();
@@ -11,10 +12,23 @@ export const useGetWorkspace = () => {
             setInterceptorReady(true);
         }
     }, [header]);
-    console.log(header);
     return useQuery({
         queryKey:['workspaces'],
         queryFn:getWorkspaces,
         enabled:!!header && isInterceptorReady,
     });
+}
+
+export const useWorkspaceNavigation=()=>{
+    const navigate=useNavigate();
+    const {data:workspaces,isLoading} = useGetWorkspace();
+    useEffect(()=>{
+        if(isLoading) return;
+        if(workspaces){
+           const hasWorkspace=workspaces.length > 0;
+           const targetPath=hasWorkspace?`/workspaces/${workspaces[0].id}`
+           :'/create';
+           navigate(targetPath);
+        }
+    },[isLoading, navigate, workspaces]);
 }
