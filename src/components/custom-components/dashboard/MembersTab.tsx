84ef34f546300c3a8afac704df863a04966ad6fe
saksham.s <sakshams982@gmail.com>
2025-02-10 15:06:52 +0545

    Table UI fix (potential change)

--- src/components/custom-components/dashboard/MembersTab.tsx
+++ src/components/custom-components/dashboard/MembersTab.tsx
@@ -5,8 +5,8 @@ import {ColumnDef} from "@tanstack/react-table";
 const MembersTab=({ columns, data }: { columns: ColumnDef<any>[]; data: unknown[]})=>{
     return <>
         <div className={'grid grid-cols-1 gap-5 h-full overflow-hidden'}>
-                    <div className=" h-full  w-full overflow-auto ">
-                        <DataGrid columns={columns} data={data}/>
+                    <div className=" h-full  w-auto overflow-auto ">
+                        <DataGrid columns={columns} data={data} search={true} searchValue={'name'} />
                     </div>
 
 
--- src/components/custom-components/shared/DataGrid.tsx
+++ src/components/custom-components/shared/DataGrid.tsx
@@ -1,102 +1,121 @@
 import {
     ColumnDef,
+    ColumnFiltersState,
     flexRender,
     getCoreRowModel,
+    getFilteredRowModel,
     useReactTable,
 } from "@tanstack/react-table"
 
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table.tsx"
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow,} from "@/components/ui/table.tsx"
 import {cn} from "@/lib/utils.ts";
+import {Input} from "@/components/ui/input.tsx";
+import {useState} from "react";
 
 interface DataTableProps<TData> {
     columns: ColumnDef<any>[]
-    data: TData[]
+    data: TData[],
+    search?: boolean,
+    searchValue?: string
 }
 
 export function DataGrid<TData>({
                                     columns,
                                     data,
+    search =false, searchValue = ''
                                 }: DataTableProps<TData>) {
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
     const table = useReactTable({
         data,
         columns,
         getCoreRowModel: getCoreRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        state: {
+            columnFilters,
+        },
     });
 
     return (
-        <div className="w-full h-full flex flex-col">
-            <div className="w-full">
-                <div className="[&_table]:w-full">
-                    <Table>
-                        <TableHeader className="bg-secondary">
-                            {table.getHeaderGroups().map((headerGroup) => (
-                                <TableRow key={headerGroup.id}>
-                                    {headerGroup.headers.map((header) => (
-                                        <TableHead
-                                            key={header.id}
-                                            className={cn('first:rounded-tl-lg last:rounded-tr-lg p-4 text-black')}
-                                            style={{
-                                                minWidth: header.column.columnDef.size,
-                                                maxWidth: header.column.columnDef.size,
-                                            }}
-                                        >
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                    header.column.columnDef.header,
-                                                    header.getContext()
-                                                )}
-                                        </TableHead>
-                                    ))}
-                                </TableRow>
-                            ))}
-                        </TableHeader>
-                    </Table>
-                </div>
-            </div>
-            <div className="flex-grow overflow-y-auto scrollbar">
-                <div className="[&_table]:w-full">
-                    <Table>
-                        <TableBody>
-                            {table.getRowModel().rows.length ? (
-                                table.getRowModel().rows.map((row) => (
-                                    <TableRow
-                                        key={row.id}
-                                        data-state={row.getIsSelected() && "selected"}
-                                    >
-                                        {row.getVisibleCells().map((cell) => (
-                                            <TableCell
-                                                key={cell.id}
-                                                className="p-2"
+        <>
+            <div className="w-full h-full flex flex-col gap-3">
+                {search &&
+                    ( <Input className={'w-full md:w-[260px] ml-auto '}
+                             value={(table.getColumn(searchValue)?.getFilterValue() as string) ?? ""}
+                             onChange={(event) =>
+                                 table.getColumn(searchValue)?.setFilterValue(event.target.value)
+
+                             }
+                             placeholder={`Search by ${searchValue}`}
+                           />
+                    )
+                }
+                <div className="flex-grow overflow-y-auto scrollbar min-h-[200px]">
+                    <div className="[&_table]:w-full ">
+                        <Table>
+                            <TableHeader className="bg-secondary">
+                                {table.getHeaderGroups().map((headerGroup) => (
+                                    <TableRow key={headerGroup.id}>
+                                        {headerGroup.headers.map((header) => (
+                                            <TableHead
+                                                key={header.id}
+                                                className={cn('first:rounded-tl-lg last:rounded-tr-lg p-4 text-black')}
                                                 style={{
-                                                    minWidth: cell.column.columnDef.size,
-                                                    maxWidth: cell.column.columnDef.size,
+                                                    minWidth: header.column.columnDef.size,
+                                                    maxWidth: header.column.columnDef.size,
                                                 }}
                                             >
-                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                            </TableCell>
+                                                {header.isPlaceholder
+                                                    ? null
+                                                    : flexRender(
+                                                        header.column.columnDef.header,
+                                                        header.getContext()
+                                                    )}
+                                            </TableHead>
                                         ))}
                                     </TableRow>
-                                ))
-                            ) : (
-                                <TableRow>
-                                    <TableCell colSpan={columns.length} className="text-center">
-                                        No results.
-                                    </TableCell>
-                                </TableRow>
-                            )}
-                        </TableBody>
-                    </Table>
+                                ))}
+                            </TableHeader>
+                        </Table>
+                    </div>
+                </div>
+                <div className="flex-grow overflow-y-auto scrollbar">
+                    <div className="[&_table]:w-full  ">
+                        <Table>
+                            <TableBody>
+                                {table.getRowModel().rows.length ? (
+                                    table.getRowModel().rows.map((row) => (
+                                        <TableRow
+                                            key={row.id}
+                                            data-state={row.getIsSelected() && "selected"}
+                                        >
+                                            {row.getVisibleCells().map((cell) => (
+                                                <TableCell
+                                                    key={cell.id}
+                                                    className="p-2"
+                                                    style={{
+                                                        minWidth: cell.column.columnDef.size,
+                                                        maxWidth: cell.column.columnDef.size,
+                                                    }}
+                                                >
+                                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                                </TableCell>
+                                            ))}
+                                        </TableRow>
+                                    ))
+                                ) : (
+                                    <TableRow>
+                                        <TableCell colSpan={columns.length} className="text-center">
+                                            No results.
+                                        </TableCell>
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                        </Table>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
