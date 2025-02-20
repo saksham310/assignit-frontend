032f5d7c7ad63dc212bf0bdcf4a8efe4f8ea0be4
sakshams310 <sakshams982@gmail.com>
2025-02-20 21:02:41 +0545

    UI changes for shadcn components

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -50,8 +50,8 @@ function Calendar({
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
-          "day-outside text-muted-foreground aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
-        day_disabled: "text-muted-foreground opacity-50",
+          "day-outside text-gray-500 aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
+        day_disabled: "text-gray-500 opacity-50",
         day_range_middle:
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
